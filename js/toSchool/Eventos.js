a06d9409d7c38d345ec49e54cd6d0d506cca167e
Paocastro <ipao526@gmail.com>
2019-01-26 18:42:18 -0500

    cambios de eventos

--- js/toSchool/Eventos.js
+++ js/toSchool/Eventos.js
@@ -1,61 +1,94 @@
 var app = angular.module('appEventos', []);
-app.controller('ctrlEventos', function($http,$scope) {
-    $scope.eventos = ['hola','chao'];
+app.controller('ctrlEventos', function ($http, $scope) {
+    $scope.eventos = [];
 
     $http({
         method: 'GET',
-        url: 'http://localhost:3000/EventosByTipo/LLS/1'
-      }).then(function successCallback(response) {
-          
-          $scope.eventos=response.data
-          console.log($scope.eventos)
-        }, function errorCallback(response) {
-            console.log(response)
-        });
-    
-        $scope.getmes = function(mes) {
-           if(mes!== undefined){
-            switch (mes) {
-                case 1:
-                    day = "Enero";
-                    break;
-                case 2:
-                    day = "Febrero";
-                    break;
-                case 3:
-                    day = "Marzo";
-                    break;
-                case 4:
-                    day = "Abril";
-                    break;
-                case 5:
-                    day = "Mayo";
-                    break;
-                case 6:
-                    day = "Junio";
-                    break;
-                    case 7:
-                    day = "Julio";
-                    break;
-                    case 8:
-                    day = "Agosto";
-                    break;
-                    case 9:
-                    day = "Septiembre";
-                    break;
-                    case 10:
-                    day = "Octubre";
-                    break;
-                    case 11:
-                    day = "Noviembre";
-                    break;
-                    case 12:
-                    day = "Diciembre";
-                    break;
+        url: 'http://localhost:5002/PageController/getEventos/LLS/EVE'
+    }).then(function successCallback(response) {
+
+        if (response.data.Respuesta.EVENTOS !== undefined) {
+            var eventos = JSON.stringify(response.data.Respuesta)
+            eventos = eventos.split('@').join('')
+            eventos = JSON.parse(eventos)
+            //var desired = eventos.replace(/[^\w\s]/gi, '')
+            //response.data.Respuesta.EVENTOS=eventos
+            if (eventos.EVENTOS.length === undefined) {
+                $scope.eventos = new Array(eventos.EVENTOS);
+            } else {
+                $scope.eventos = eventos.EVENTOS;
             }
-            return day;
-           }
+
+        }
+        console.log($scope.eventos)
+    }, function errorCallback(response) {
+        console.log(response)
+    });
+
+    $scope.getHora = function (fecha) {
+        console.log(fecha)
+        var hora;
+        var minutos;
+        var time= fecha.substring(11, 16);
+        hora= time.substring(0,2)
+        minutos=time.substring(3,6)
+        console.log(minutos)
+        if(parseInt(hora)>12){
+            hora=parseInt(hora)-12;
+            time= hora + ":" + minutos + " P.M"
+        }else{
+            time= hora + ":" + minutos + " A.M"
+        }
         
+        return time;
+        //var time = fecha.toLocaleTimeString();
+        
+    }
+    $scope.getmes = function (mes) {
+        
+        if (mes !== undefined) {
+            switch (mes) {
+                case "1":
+                    return "Enero";
+                    break;
+                case "2":
+                    return "Febrero";
+                    break;
+                case "3":
+                    return "Marzo";
+                    break;
+                case "4":
+                    return "Abril";
+                    break;
+                case "5":
+                    return "Mayo";
+                    break;
+                case "6":
+                    return "Junio";
+                    break;
+                case "7":
+                    return "Julio";
+                    break;
+                case "8":
+                    return "Agosto";
+                    break;
+                case "9":
+                    return "Septiembre";
+                    break;
+                case "10":
+                    return "Octubre";
+                    break;
+                case "11":
+                    return "Noviembre";
+                    break;
+                case "12":
+                    return "Diciembre";
+                    break;
+                    
+            }
+           
+        }
+
     }
 });
 
